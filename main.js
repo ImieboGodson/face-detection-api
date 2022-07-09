b8a663e49b966d1adef1d91c55a3d3eafad23b54
Godson <imiebo.godson@gmail.com>
2022-07-09 21:24:38 +0100

    set up initial files

--- main.js
+++ main.js
@@ -1,10 +1,54 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
+const PORT = 3001;
+
+const db = {
+	users: [
+		{
+			id: 001,
+			name: 'Jefferson Grey',
+			email: 'jeff@sample.com',
+			password: 'beans',
+			entries: 5,
+			followers: [ 002, 003 ],
+			joined: new Date(),
+		},
+		{
+			id: 002,
+			name: 'Adewale James',
+			email: 'adewale@sample.com',
+			password: 'rice',
+			entries: 3,
+			followers: [ 001 ],
+			joined: new Date(),
+		},
+		{
+			id: 003,
+			name: 'Kaleb Jones',
+			email: 'jones@sample.com',
+			password: 'cats',
+			entries: 3,
+			followers: [ 001, 002 ],
+			joined: new Date(),
+		},
+	]
+}
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+
+//HOME ROUTE
 app.get('/', (req, res) => {
-	res.send('Hello World, I am alive!');
+	res.send(db.users);
 })
 
-app.listen(3000, () => {
-	console.log('Server is live!!');
+
+
+app.listen(PORT, () => {
+	console.log(`Server is live on PORT ${PORT}!!`);
 });
