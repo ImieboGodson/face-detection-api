e9c8c87360c5e59439b30e020992dc03b74d63f3
Godson Imiebo <imiebo.godson@gmail.com>
2022-07-24 02:30:18 +0100

    upgrade from old clarifai nodejs version to new version with grpc

--- controllers/image.js
+++ controllers/image.js
@@ -1,19 +1,42 @@
-const Clarifai = require('clarifai');
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 
-const app = new Clarifai.App({
-	apiKey: process.env.API_KEY
-});
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", `Key ${process.env.API_KEY}`);
 
 const handleApiCall = () => (req, res) => {
 	const { input } = req.body;
 
-	app.models.predict (Clarifai.FACE_DETECT_MODEL, input)
-		.then(data => {
-			res.send(data);
-		})
-		.catch(err => {
-			res.status(400).json('Error Fetching Image Data');
-		})
+	stub.PostModelOutputs(
+		{
+			// This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+			model_id: "a403429f2ddf4b49b307e318f00e528b",
+			inputs: [{data: {image: {url: input}}}]
+		},
+		metadata,
+		(err, response) => {
+			if (err) {
+				console.log("Error: " + err);
+				return;
+			}
+	
+			if (response.status.code !== 10000) {
+				console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+				return;
+			}
+
+			res.send(response);
+		}
+	);
+
+	// app.models.predict (Clarifai.FACE_DETECT_MODEL, input)
+	// 	.then(data => {
+	// 		res.send(data);
+	// 	})
+	// 	.catch(err => {
+	// 		res.status(400).json('Error Fetching Image Data');
+	// 	})
 }
 
 
