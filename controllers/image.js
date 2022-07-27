e6e521550112d22ecfcee7e12bc63c9fa527b4b2
Godson Imiebo <imiebo.godson@gmail.com>
2022-07-27 16:28:10 +0100

    added validation to image route, if input field is empty user wont be able to call endpoint

--- controllers/image.js
+++ controllers/image.js
@@ -7,36 +7,31 @@ metadata.set("authorization", `Key ${process.env.API_KEY}`);
 
 const handleApiCall = () => (req, res) => {
 	const { input } = req.body;
-
-	stub.PostModelOutputs(
-		{
-			// This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-			model_id: "a403429f2ddf4b49b307e318f00e528b",
-			inputs: [{data: {image: {url: input}}}]
-		},
-		metadata,
-		(err, response) => {
-			if (err) {
-				console.log("Error: " + err);
-				return;
-			}
+	if(input === '') {
+		res.status(400).json('Input Field is Empty');
+	} else {
+		stub.PostModelOutputs(
+			{
+				// This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+				model_id: "a403429f2ddf4b49b307e318f00e528b",
+				inputs: [{data: {image: {url: input}}}]
+			},
+			metadata,
+			(err, response) => {
+				if (err) {
+					console.log("Error: " + err);
+					return;
+				}
+		
+				if (response.status.code !== 10000) {
+					console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+					return;
+				}
 	
-			if (response.status.code !== 10000) {
-				console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-				return;
+				res.send(response);
 			}
-
-			res.send(response);
-		}
-	);
-
-	// app.models.predict (Clarifai.FACE_DETECT_MODEL, input)
-	// 	.then(data => {
-	// 		res.send(data);
-	// 	})
-	// 	.catch(err => {
-	// 		res.status(400).json('Error Fetching Image Data');
-	// 	})
+		);
+	}
 }
 
 
